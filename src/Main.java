dc79d4083044ed48d067abb67757c0015f5d1721
Дмитрий Петинов <dpetinov@ibs.ru>
2024-04-21 11:22:53 +0400

    JavaBasic Final Task 3

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        System.out.print("Количество строк: ");
+        int linesCount = console.nextInt();
+        String[] lines = new String[linesCount];
+        int maxUniqueLetters = 0;
+        String mostVariableLine = "";
+        Scanner console2 = new Scanner(System.in);
+        for (int i = 0; i < linesCount; i++) {
+            StringBuilder uniqueLetters = new StringBuilder();
+            System.out.print("Строка " + (i + 1) + ": ");
+            String currentLine = console2.nextLine();
+            lines[i] = currentLine;
+            for (int j= 0; j < currentLine.length(); j++) {
+                if (!uniqueLetters.toString().contains(String.valueOf(currentLine.charAt(j)))) {
+                    uniqueLetters.append(currentLine.charAt(j));
+                }
+            }
+            if (maxUniqueLetters < uniqueLetters.toString().length()) {
+                maxUniqueLetters = uniqueLetters.toString().length();
+                mostVariableLine = lines[i];
+            }
+        }
+        System.out.println("Ответ: " + mostVariableLine);
+    }
+}
